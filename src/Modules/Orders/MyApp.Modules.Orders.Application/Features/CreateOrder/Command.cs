eb1d80a7360e3398645e00d2ffd9644c22fe5ac7
radseq <radek.g.10@gmail.com>
2026-03-07 16:08:25 +0100

    #feat: remove unneeded result of creating order

--- src/Modules/Orders/MyApp.Modules.Orders.Application/Features/CreateOrder/Command.cs
+++ src/Modules/Orders/MyApp.Modules.Orders.Application/Features/CreateOrder/Command.cs
@@ -7,7 +7,7 @@ public sealed record Command(
 	int CustomerId,
 	DateTime? OrderDateUtc,
 	List<CreateOrderItemRequest> Items
-) : ICommand<CreateOrderAndDispatchTransportResponse>, ISkipUnitOfWorkBehavior;
+) : ICommand;
 
 // ISkipUnitOfWorkBehavior or ITransactionalCommand etc\
 // ISkipUnitOfWorkBehavior by nie robi³ automatycznie begin async, save async, commit, rollback
--- src/Modules/Orders/MyApp.Modules.Orders.Application/Features/CreateOrder/Handler.cs
+++ src/Modules/Orders/MyApp.Modules.Orders.Application/Features/CreateOrder/Handler.cs
@@ -15,35 +15,35 @@ public sealed class Handler(
         Command request, CancellationToken cancellationToken)
     {
         if (request.Items is null || request.Items.Count == 0)
-            return MessageResult<CreateOrderAndDispatchTransportResponse>.Fail(Errors.Orders.EmptyItems);
+            return MessageResult.Fail(Errors.Orders.EmptyItems);
 
         if (!await customers.ExistsAsync(request.CustomerId, cancellationToken))
-            return MessageResult<CreateOrderAndDispatchTransportResponse>.Fail(Errors.Customers.NotFound(request.CustomerId));
+            return MessageResult.Fail(Errors.Customers.NotFound(request.CustomerId));
 
         var items = new List<OrderItem>();
         foreach (var r in request.Items)
         {
             var it = OrderItem.Create(r.ProductId, r.UnitPrice, r.Quantity);
             if (!it.IsSuccess)
-                return MessageResult<CreateOrderAndDispatchTransportResponse>.Fail(it.PrimaryError!);
+                return MessageResult.Fail(it.PrimaryError!);
 
             items.Add(it.Value!);
         }
 
         var orderRes = Order.Create(request.CustomerId, request.OrderDateUtc ?? DateTime.UtcNow, items);
         if (!orderRes.IsSuccess)
-            return MessageResult<CreateOrderAndDispatchTransportResponse>.Fail(orderRes.PrimaryError!);
+            return MessageResult.Fail(orderRes.PrimaryError!);
 
         var order = orderRes.Value!;
 
         var confirm = order.Confirm();
         if (!confirm.IsSuccess)
-            return MessageResult<CreateOrderAndDispatchTransportResponse>.Fail(confirm.PrimaryError!);
+            return MessageResult.Fail(confirm.PrimaryError!);
 
         var saved = await orders.AddAsync(order, cancellationToken);
         if (saved.HasFailed)
         {
-            return MessageResult<CreateOrderAndDispatchTransportResponse>.Fail(saved.Errors);
+            return MessageResult.Fail(saved.Errors);
         }
 
         return MessageResult.Ok();
